2fc60649fe47bf6bdcbf64e4da673e0f629e6f37
Prissadang Suta <prissadang@kdlab.ai>
2021-02-08 11:11:08 +0700

    fixed email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,6 +19,7 @@ $email_subject = "SITA Website Contact Form:  $name";
 $email_body = "You have received a new message from SITA website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+
+mail($to, $email_subject, $email_body, $headers);
 return true;			
 ?>
